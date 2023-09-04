a604003d83a16c8835fb9286b5bf5eb24ae6aa42
Smart-ibanga <ibangasmart@gmail.com>
2023-09-04 05:47:56 +0100

    speakers data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import {data} from ".../SpeakerData";
+
+ const {id, first, last,company,bio,twitterHandle,favourite, sessions,} = data[0];
+
 const IndexPage = () => {
     return(
-        <div>Hello from Pluralsight</div>
+        <div className="container speakers-list">
+            <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+                    
+                </div>
+            </div>
+        </div>
     );
      
 }
