ecb0fcadcd5455147cea282af1be83b459e2f1b8
Smart-ibanga <ibangasmart@gmail.com>
2023-09-08 02:13:24 +0100

    Creating Speaker component

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,14 @@
 import {data} from "../SpeakerData";
+import Speaker from "../src/components/Speaker"
 
-function Session({ title, room }) {
-  return (
-    <span className="session w-100">
-      {title} <strong>Room:{room}</strong>
-    </span>
-  );
-}
-
-function Sessions({sessions}) {
-  return (
-    <div className="sessionBox card h-250">
-      <Session title={session[0].title} room={sessions[0].room.name} />
-    </div>
-  );
-}
 
 const IndexPage = () => {
     return (
       <div className="container speakers-list">
         <div className="row">
           {data.map(function (speaker) {
-            const {
-              id,
-              first,
-              last,
-              company,
-              bio,
-              twitterHandle,
-              favourite,
-              sessions,
-            } = speaker;
-
             return (
-              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
-                <div className="card card-height p-4 mt-4">
-                  <div className="speaker-img d-flex flex-row justify-content-center align-item-center h-300">
-                    <img
-                      className="contain-fit"
-                      src={`/images/speaker-${id}.jpg`}
-                      width="300"
-                      alt={`${first} ${last}`}
-                    />
-                  </div>
-                  <div className="speaker-info">
-                    <div className="d-flex justify-content-between mb-3">
-                      <h3 className="text-truncate w-200">
-                        {first} {last}
-                      </h3>
-                    </div>
-                    <div>
-                      <p>
-                        {bio} {company} {twitterHandle} {favourite}
-                      </p>
-                    </div>
-                  </div>
-                  < Sessions sessions={sessions} />
-                </div>
-              </div>
+             <Speaker key={speaker.id} speaker={speaker} />
             );
           })}
         </div>
@@ -66,4 +17,4 @@ const IndexPage = () => {
      
 }
 
-export default IndexPage
+export default IndexPage;
